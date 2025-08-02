b2ebd5a3f46ac6c69fc9fafa79b82d48835e5d22
Liathakral <132537723+Liathakral@users.noreply.github.com>
2025-08-02 11:45:55 +0530

    2nd commit

--- functions/App.mjs
+++ functions/App.mjs
@@ -2,13 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import router from './routes/api.mjs';
+import router from '../routes/api.mjs';
+import serverless from 'serverless-http';
+
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect(
-  `mongodb+srv://LiaThakral:Liathakral%4028@cluster0.pfx3dib.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`,
+mongoose.connect(process.env.MONGODB_URI, 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -32,5 +33,4 @@ app.use('/api', router);
 app.get('/', (req, res) => {
   res.send('Server is live!');
 });
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export const handler = serverless(app);
